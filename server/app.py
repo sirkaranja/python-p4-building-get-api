d0cc1c56c014641efd37a48406286af314b6d9c0
daniel <sirkaranja12@gmail.com>
2023-06-27 12:05:49 +0300

    building get api

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
@@ -19,5 +18,25 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),
+        200
+    )
+
+    return response
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
